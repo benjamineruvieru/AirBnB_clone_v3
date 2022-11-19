ba9580e28038bfa38298cd65d581c7bb71e0e0cf
ben <benjamineruvieru@gmail.com>
2022-11-19 23:25:14 +0100

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,8 +6,9 @@ Flask App that integrates with AirBnB static HTML Template
 
 from api.v1.views import app_views
 from models import storage
-from flask import Flask
+from flask import Flask, jsonify, make_response
 import os
+
 
 app = Flask(__name__)
 
@@ -27,6 +28,21 @@ def close(exception):
     the current SQLAlchemy Session
     """
     storage.close()
+
+
+@app.errorhandler(Exception)
+def global_error_handler(err):
+    """
+    Global Route to handle All Error Status Codes
+    """
+    if err.code is 404:
+        err.description = "Not found"
+        message = {'error': err.description}
+        code = err.code
+    else:
+        message = {'error': err}
+        code = 500
+    return make_response(jsonify(message), code)
 
 
 if __name__ == '__main__':
