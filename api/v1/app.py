8ec34f1b39aad0d471f0e147b6e5e43f6bdcdad5
ben <benjamineruvieru@gmail.com>
2022-11-19 22:40:14 +0100

    task 3

---
+++ api/v1/app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+Flask App that integrates with AirBnB static HTML Template
+"""
+
+from api.v1.views import app_views
+from models import storage
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
+# flask server environmental setup
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close(exception):
+    """
+    this method calls .close() 
+    the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    """
+    MAIN Flask App
+    """
+    app.run(host=host, port=port, threaded=True)
